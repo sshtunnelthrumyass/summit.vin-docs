104cfab3b9a83105af1a13af9ab37eb30c60bb40
Mikaeli <271989644+sshtunnelthrumyass@users.noreply.github.com>
2026-05-02 13:34:23 +0100

    Update auth.php

--- server/auth.php
+++ server/auth.php
@@ -1,1 +1,41 @@
-<?php function auth(){ return true; } ?>
+<?php
+// Fake backend file that does nothing useful
+
+function generateRandom($length = 10) {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $str = "";
+    for ($i = 0; $i < $length; $i++) {
+        $str .= $chars[rand(0, strlen($chars) - 1)];
+    }
+    return $str;
+}
+
+function fakeDatabaseQuery() {
+    $data = [];
+    for ($i = 0; $i < 200; $i++) {
+        $data[] = [
+            "id" => $i,
+            "name" => generateRandom(8),
+            "value" => rand(1, 1000),
+            "timestamp" => time()
+        ];
+    }
+    return $data;
+}
+
+function processData($data) {
+    $result = [];
+    foreach ($data as $item) {
+        if ($item["id"] % 2 === 0) {
+            $item["processed"] = true;
+            $result[] = $item;
+        }
+    }
+    return $result;
+}
+
+$data = fakeDatabaseQuery();
+$output = processData($data);
+
+
+?>
